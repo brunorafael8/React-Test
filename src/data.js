5cc15d01a6326a170589bb2cf271c10adbcb6e4b
Bruno Rafael <bruninho_lito@hotmail.com>
2017-07-21 11:51:28 -0300

    data add

---
+++ src/data.js
@@ -0,0 +1,46 @@
+export default {
+  pictures: [
+    {
+      'id': 0,
+      'url': 'https://upload.wikimedia.org/wikipedia/en/c/c9/Hybrid_Theory_EP.jpg',
+			'name': 'Hybrid Theory EP',
+			'released': 'May 1, 1999'
+		},    
+		{
+      'id': 1,
+      'url': 'https://upload.wikimedia.org/wikipedia/en/c/c9/Linkin_park_hybrid_theory.jpg',
+			'name': 'Hybrid Theory',
+			'released': 'October 24, 2000'
+		},
+		{
+      'id': 2,
+      'url': 'https://upload.wikimedia.org/wikipedia/en/6/64/MeteoraLP.jpg',
+			'name': 'Meteora',
+			'released': 'March 25, 2003'
+		},
+		{
+      'id': 3,
+      'url': 'https://upload.wikimedia.org/wikipedia/en/a/a2/LP-LiveInTexas.jpg',
+			'name': 'Live in Texas',
+			'released': 'November 18, 2003'
+		},		
+		{
+      'id': 4,
+      'url': 'https://upload.wikimedia.org/wikipedia/en/7/7a/Minutes_to_Midnight_cover.jpg',
+			'name': 'Minutes to Midnight',
+			'released': 'May 14, 2007'
+		},
+		{
+      'id': 5,
+      'url': 'https://upload.wikimedia.org/wikipedia/en/a/af/A_Thousand_Suns_Cover2.jpg',
+			'name': 'A Thousand Suns',
+			'released': 'September 8, 2010'
+		},		
+		{
+      'id': 6,
+      'url': 'https://upload.wikimedia.org/wikipedia/en/c/c9/Linkin_Park_-_Living_Things.jpg',
+			'name': 'Living Thingst',
+			'released': '	June 20, 2012'
+    },
+  ]
+}
