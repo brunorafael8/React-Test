d79ce5eec929bb99e11df61218c99da0aeebbf73
Bruno Rafael <bruninho_lito@hotmail.com>
2017-07-26 17:42:36 -0300

    Details story delete

--- storybook/stories/Details.story.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { storiesOf } from '@kadira/storybook';
-import EpsApi from 'src/data'
-
-import Details from 'src/components/details/Details'
-
-const stories = storiesOf('Details', module);
-
-stories.add('default', () => <Details />	);
